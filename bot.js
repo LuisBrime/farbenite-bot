badb9774abd6770cc20e0dfdb1aef64baf43c929
Luis Brime <brimegmz@gmail.com>
2019-08-01 21:03:32 -0500

    again :shipit:

--- bot.js
+++ bot.js
@@ -17,7 +17,6 @@ const params = {
 
 tweet();
 
-
 function tweet() {
     axios(params)
         .then(response => {
@@ -59,27 +58,6 @@ function tweet() {
                 }
             });
         });
-
-    /*
-    fs.readFile('./utils/palettes.json', 'utf8', function(err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            let obj = JSON.parse(data);
-            obj.palettes.push({ name: "holi" });
-            const json = JSON.stringify(obj);
-            fs.writeFile('./utils/palettes.json', json, 'utf8', null);
-        }
-    });
-    */
-
-    T.post('statuses/update', t, function(err, data, response) {
-        if (err) {
-            console.log('Cannot tweet, ', err);
-        } else {
-            console.log('Success, ', data);
-        }
-    });
 }
 
 const rgbToHex = (rgb) => {
