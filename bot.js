fb022b65e02324de6ac8471773156fcff100bd4f
Luis Brime <brimegmz@gmail.com>
2019-10-03 21:10:32 -0500

    hope this works, changed way to execute commands

--- bot.js
+++ bot.js
@@ -1,12 +1,16 @@
 var Twit = require('twit');
 var config = require('./config');
 var axios = require('axios');
-var exec = require('child_process').exec;
 var fs = require('fs');
+const { spawn, exec } = require('child_process');
 
 var T = new Twit(config);
 
+// Example command for Processing to create palette image.
+// ./farbenite/farbenite 0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 F F F F F
 var cmd = './farbenite/farbenite ';
+
+// params to make the request to get the palette's colors.
 const params = {
     method: 'post',
     url: 'http://colormind.io/api/',
@@ -15,24 +19,31 @@ const params = {
     }
 };
 
+// Tweet a image and set an interval to tweet.
 tweetImage();
-setInterval(tweetImage, 1000*60*60*2);
+setInterval(tweetImage, 1000*60*60*5);
 
+// Create a stream that will track all events where '@farbenite' appears.
 var stream = T.stream('statuses/filter', { track: ['@farbenite'] });
-
+// When a tweet with '@farbenite' appears, do the mention function.
 stream.on('tweet', mention);
 
+// Tweet an image given a palette.
 function tweetImage() {
+    // Make the request to Colormind.
     axios(params)
         .then(response => {
             console.log('––– COLORMIND REQUEST SUCCESS –––\n\t', response.data.result);
+            // RGB colors
             const colors = response.data.result;
-            let hexs = [];
 
+            // Turn those RGB colors to HEX.
+            let hexs = [];
             for (var i = 0; i < colors.length; i++) {
                 hexs[i] = fullRGB2HEX(colors[i][0], colors[i][1], colors[i][2]);
             }
 
+            // Add the colors to the command for Processing.
             cmd += colors[0][0] + ' ' + colors[0][1] + ' ' + colors[0][2] + ' '
                 + colors[1][0] + ' ' + colors[1][1] + ' ' + colors[1][2] + ' '
                 + colors[2][0] + ' ' + colors[2][1] + ' ' + colors[2][2] + ' '
@@ -40,30 +51,48 @@ function tweetImage() {
                 + colors[4][0] + ' ' + colors[4][1] + ' ' + colors[4][2] + ' '
                 + hexs[0] + ' ' + hexs[1] + ' ' + hexs[2] + ' ' + hexs[3] + ' ' + hexs[4];
             
-            exec('sudo nohup Xvfb :1 -screen 0 1024x768x24 &', function() {
-                exec('export DISPLAY=":1"', function() {
-                    exec(cmd, function() {
-                        console.log('––– PROCESSING IMAGE ENDED –––');
-                        const filename = './farbenite/output.png';
-                        var en = { encoding: 'base64' };
-                        var b64 = fs.readFileSync(filename, en);
-        
-                        T.post('media/upload', { media_data: b64 }, uploaded);
-                        function uploaded(err, data, response) {
-                            console.log('––– MEDIA UPLOADED –––');
-                            var id = data.media_id_string;
-                            var tuit = {
-                                    media_ids: [id]
-                            };
-                            
-                            tweet(tuit);
-                        }
-                    });
+            //// TRICKY PART ////
+            // Executing command to create fake display on the EC2 instance,
+            // if there's no display, Processing command wont work.
+            exec('sudo Xvfb :1 -screen 0 1024x768x24 | export DISPLAY=":1"', function(err, stdout, stderr) {
+                if (err) {
+                    console.error(`exec error: ${err}`);
+                }
+
+                const child = spawn(cmd, { stdio: 'inherit', shell: true });
+
+                // Log anything Processing has to tell us.
+                child.stdout.on('data', (data) => {
+                    console.log(`Processing stdout: \n${data}`);
                 });
-            });
+                // If error occurs in Processing return so the bot doesn't tweet garbage image.
+                child.stderr.on('data', (data) => {
+                    console.error(`Processing error: \n${data}`);
+                    return;
+                });
+
+                console.log('––– PROCESSING IMAGE ENDED –––');
+                // Enocde image to upload it
+                const filename = './farbenite/output.png';
+                var en = { encoding: 'base64' };
+                var b64 = fs.readFileSync(filename, en);
+
+                // Upload image and then tweet it.
+                T.post('media/upload', { media_data: b64 }, uploaded);
+                function uploaded(err, data, response) {
+                    console.log('––– MEDIA UPLOADED –––');
+                    var id = data.media_id_string;
+                    var tuit = {
+                            media_ids: [id]
+                    };
+                    
+                    tweet(tuit);
+                }
+                });
         });
 }
 
+// Function to tweet any given tweet.
 function tweet(tuit) {
     T.post('statuses/update', tuit, tweeted);
 
@@ -76,6 +105,7 @@ function tweet(tuit) {
     }
 }
 
+// Function to respond to a tweet mention to '@farbenite'.
 function mention(tweet) {
     let inReplyTo = tweet.in_reply_to_screen_name;
     if (inReplyTo === 'farbenite') {
@@ -83,15 +113,18 @@ function mention(tweet) {
         let id = tweet.id_str;
         let username = tweet.user.screen_name;
 
+        // Favorite mention tweet.
         T.post('favorites/create', { id });
     }
 }
 
-function goAndFollow(tweet) {
-    let username = tweet.user.screen_name;
-    T.post('friendships/create', { screen_name: username });
-}
-
+/*
+ *
+ *
+ * UTILITY FUNCTIONS
+ * 
+ * 
+*/
 const rgbToHex = (rgb) => {
     var hex = Number (rgb).toString(16);
     if (hex.length < 2) {
